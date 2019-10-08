e5aa79f31bbb9323b08537cfcd8b81e66f7f4861
Annie <gichiraanne@gmail.com>
2019-10-08 08:53:49 +0300

    Add sample data to app

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,13 +1,31 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev'
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+	noInfo: true,
+	publicPath: config.output.publicPath
+}));
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../src/index.html'))
 });
+
+
+app.get('/users', (req, res) => {
+	res.json([
+		{"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@mail.com"},
+		{"id": 2, "firstName": "Jane", "lastName": "Will", "email": "Jane@mail.com"},
+		{"id": 3, "firstName": "Anne", "lastName": "Olympia", "email": "Olympia10@gmail.com"}
+	]);
+});
+
 
 app.listen(port, (err) => {
 	if (err) {
