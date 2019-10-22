1a08e603be50c46f7f059672054bebce87f5ff90
Annie <gichiraanne@gmail.com>
2019-10-22 11:09:08 +0300

    User Api and data display on index

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,14 @@
 import 'whatwg-fetch';
+import { getbaseUrl } from './baseUrl';
+
+const baseUrl = getbaseUrl();
 
 export function getUsers() {
 	return get('users')
 }
 
 function get(url){
-	return fetch(url).then(onSuccess, onError);
+	return fetch(baseUrl + url).then(onSuccess, onError);
 }
 
 function onSuccess(response){
